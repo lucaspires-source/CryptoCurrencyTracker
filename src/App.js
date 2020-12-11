55a4b8bf310710bab2816617e82586558ea503ac
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-11 13:09:46 -0300

    finished coin component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import Coin from './components/Coin'
+import Coin from "./components/Coin";
 const App = () => {
   const [coins, setCoins] = useState([]);
-  const [search,setSearch] = useState("")
+  const [search, setSearch] = useState("");
   useEffect(() => {
     axios
       .get(
@@ -16,20 +16,41 @@ const App = () => {
       })
       .catch((error) => console.log(error));
   }, []);
-  const handleChange =  e =>{
-    setSearch(e.target.value)
-  }
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <div className="coin-app">
       <div className="coin-search">
-          <h1 className="coin-text">Search a Currency</h1>
-          <form>
-            <input type="text" placeholder="Search..." className="coin-input"/>
-          </form>
+        <h1 className="coin-text">Search a Currency</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="coin-input"
+            onChange={handleChange}
+          />
+        </form>
       </div>
-      
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
     </div>
-
   );
 };
 
