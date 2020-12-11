1d5bd7c42b17ba25b0a4c95f0ba555a320e63bee
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-11 11:02:32 -0300

    installing axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.css';
+import axios from 'axios'
 
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=100&page=1&sparkline=false //
 const  App = () => {
   return (
     <div className="App">
